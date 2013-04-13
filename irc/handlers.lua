a513134ea02b7464aeb57703ad948207640c0bbb
cracker64 <cracker642@gmail.com>
2013-04-12 20:59:27 -0400

    silly debug comments

--- irc/handlers.lua
+++ irc/handlers.lua
@@ -75,27 +75,7 @@ handlers["NICK"] = function(o, prefix, newnick)
 		o:invoke("NickChange", user, newnick)
 	end
 end
---:zelazny.freenode.net 352 Crackbot ##powder-bots ~meow Powder/Developer/cracker64 barjavel.freenode.net cracker64 H+ :0 14Wafflessssss
---[[
-verne.freenode.net	Crackbot	##powder-bots	~Meow	r75-110-131-16.nbrncmtc02.nwbrnc.ab.dh.suddenlink.net	verne.freenode.net	Crackbot	H	0 moo	nil
-verne.freenode.net	Crackbot	##powder-bots	~Mitch|hat	unaffiliated/incredible	cameron.freenode.net	Mitch|hater	H	0 Why do you read this?	nil
-verne.freenode.net	Crackbot	##powder-bots	Bott	cpe-24-170-62-161.stx.res.rr.com	niven.freenode.net	Bott	H	0 bubdan	nil
-verne.freenode.net	Crackbot	##powder-bots	~mniip	178.219.36.155	hubbard.freenode.net	mniip	H	0 mniip	nil
-verne.freenode.net	Crackbot	##powder-bots	~jacob	Powder/Developer/jacob1	morgan.freenode.net	jacob1[A]	H+	0 realname	nil
-verne.freenode.net	Crackbot	##powder-bots	~ErEnUa	WiseOS/Bot/WiseBot	card.freenode.netErEnUa	H	0 ErEnUa Bot	nil
-verne.freenode.net	Crackbot	##powder-bots	~NiaTeppel	WiseOS/Founder/NiaTeppelin	card.freenode.net	SuinDraw	H	0 realname	nil
-verne.freenode.net	Crackbot	##powder-bots	~Incredibl	modemcable139.215-23-96.mc.videotron.ca	card.freenode.net	TheBombMaker	H	0 realname	nil
-verne.freenode.net	Crackbot	##powder-bots	~meow	Powder/Developer/cracker64	barjavel.freenode.net	cracker64	H+	0 14Wafflessssss	nil
-verne.freenode.net	Crackbot	##powder-bots	~WFeliks	81-237-248-234-no113.tbcn.telia.com	barjavel.freenode.net	WFeliks	H	0 WFeliks	nil
-verne.freenode.net	Crackbot	##powder-bots	~xsBot	178.219.36.155	wolfe.freenode.net	xsBot	H@	0 xsBot	nil
-verne.freenode.net	Crackbot	##powder-bots	~evil_dan2	unaffiliated/evil-dan2wik/x-0106201	wright.freenode.net	evil_dan2wik	H	0 dan2wik	nil
-verne.freenode.net	Crackbot	##powder-bots	~huehue	WiseOS/PkgBuilder/RafaelRistovski	wright.freenode.net	[Ristovski]	H	0 HUEHUE	nil
-verne.freenode.net	Crackbot	##powder-bots	~Tribot200	unaffiliated/triclops200/bot/tribot200	hubbard.freenode.net	Tribot200	H@	0 Tribot200	nil
-verne.freenode.net	Crackbot	##powder-bots	Stewie	2a01:7e00::f03c:91ff:fedf:890f	cameron.freenode.net	StewieGriffinSub	H@	0 SG substitute, contact jacksonmj about problems	nil
-verne.freenode.net	Crackbot	##powder-bots	jacksonmj	2a01:7e00::f03c:91ff:fedf:890f	lindbohm.freenode.net	jacksonmj-away	G+	0 jacksonmj	nil
-verne.freenode.net	Crackbot	##powder-bots	ChanServ	services.	services.	ChanServ	H@	0 Channel Services	nil
-verne.freenode.net	Crackbot	##powder-bots	~Triclops2	Powder/Developer/Triclops200	asimov.freenode.net	Triclops256|away	H+	0 Nonya	nil
-]]
+--WHO list
 handlers["352"] = function(o, prefix, me, channel, name1, host, serv, name, access ,something, something2)
 	if o.track_users then
 	    local user = {nick=name, host=host, username=name1, serv=serv, access=parseWhoAccess(access)}

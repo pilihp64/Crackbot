d63bb57d5f920f9207ff5707f94faa232f52a84a
jacob1 <jfu614@gmail.com>
2013-11-03 00:48:42 -0400

    comment out lua and python functions for now ...

--- filters.lua
+++ filters.lua
@@ -339,27 +339,28 @@ function mknumscramb(n)
 end
 
 local function luaFilt(text,args)
-	local msg = args[1]
-	if not msg then return "" end
-	local realtext=""
-	if not args.skip then
-		for i=2,#args do realtext=realtext.." "..args[i] end
-		realtext = realtext:sub(2)
-	else
-		realtext=text or ""
-	end
+	return "42"
+--	local msg = args[1]
+--	if not msg then return "" end
+--	local realtext=""
+--	if not args.skip then
+--		for i=2,#args do realtext=realtext.." "..args[i] end
+--		realtext = realtext:sub(2)
+--	else
+--		realtext=text or ""
+--	end
+--
+--	msg = msg:gsub("[\"\'\\$`]","\\%1")
+--	realtext = realtext:gsub("[\"\'\\$`]","\\%1")
+--	local rf = io.popen([[lua -e "dofile('derp.lua') dofile('sandybox.lua') local e,err=load_code('return function(...) ]]..msg..[[ end',nil,'t',env) if e then local r = {pcall(e(),']]..realtext..[[')} local s = table.remove(r,1) print(unpack(r)) else print(err) end" 2>&1]])
+--	socket.sleep(0.1)
+--	local kill = io.popen([[pgrep -f "lua -e"]]):read("*a")
+--	if kill~="" then os.execute([[pkill -f "lua -e"]]) end
+--	local r = rf:read("*a")
+--	if r=="" and kill and kill~="" then r="Killed" end
+--	if r then r = r:gsub("[\r\n]"," ") end
 
-	msg = msg:gsub("[\"\'\\$`]","\\%1")
-	realtext = realtext:gsub("[\"\'\\$`]","\\%1")
-	local rf = io.popen([[lua -e "dofile('derp.lua') dofile('sandybox.lua') local e,err=load_code('return function(...) ]]..msg..[[ end',nil,'t',env) if e then local r = {pcall(e(),']]..realtext..[[')} local s = table.remove(r,1) print(unpack(r)) else print(err) end" 2>&1]])
-	socket.sleep(0.1)
-	local kill = io.popen([[pgrep -f "lua -e"]]):read("*a")
-	if kill~="" then os.execute([[pkill -f "lua -e"]]) end
-	local r = rf:read("*a")
-	if r=="" and kill and kill~="" then r="Killed" end
-	if r then r = r:gsub("[\r\n]"," ") end
-
-	return r
+--	return r
 end
 local function luaFiltSane(args,filt)
 	args.skip = filt

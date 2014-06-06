c379735055a1c6334acc89398bb22ddf413571ca
wolfy1339 <wolfy1339@users.noreply.github.com>
2014-06-06 11:10:45 -0400

    Make error for windows trying to use the line-in

--- init.lua
+++ init.lua
@@ -21,7 +21,10 @@ local socket = require"socket"
 local console=socket.tcp()
 console:settimeout(5)
 
-if not WINDOWS and config.console.input then
+if WINDOWS and config.console.input then
+	w=print("Unforunately, Windows doesn't support the console line-in")
+	error(w)
+elseif not WINDOWS and config.console.input then
 	--start my console line-in
 	os.execute(config.console.terminal.." -x lua consolein.lua")
 end
